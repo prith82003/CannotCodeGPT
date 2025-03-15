bd8951e426da7ab7699230e6f270b90da5a027de
Prithviraaj <z5419216@ad.unsw.edu.au>
2025-03-15 16:35:40 +1000

    combined chatbot

--- app/(routes)/dashboard/budgets/page.jsx
+++ app/(routes)/dashboard/budgets/page.jsx
@@ -1,12 +1,17 @@
+"use client"
 import React from 'react'
 import BudgetList from './_components/BudgetList'
+import Chat from '../Chatbot/Chat'
 
 function budgets() {
   return (
-    <div className='p-10'>
+    <>
+      <div className='p-10'>
         <h2 className='font-bold text-3xl'>My Budgets</h2>
-        <BudgetList/>
-    </div>
+        <BudgetList />
+      </div>
+      <Chat />
+    </>
   )
 }
 
--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -8,72 +8,76 @@ import { Budgets, Expenses } from '@/utils/schema';
 import BarChartDashboard from './_components/BarChartDashboard';
 import BudgetItem from './budgets/_components/BudgetItem';
 import ExpenseListTable from './expenses/_components/ExpenseListTable';
+import Chat from './Chatbot/Chat';
 
 function Dashboard() {
-  const {user}=useUser();
-  const[budgetList,setBudgetList]=useState([]);
-  const[expensesList,setExpensesList]=useState([])
-  
-  useEffect(()=>{
-      getBudgetList();
+  const { user } = useUser();
+  const [budgetList, setBudgetList] = useState([]);
+  const [expensesList, setExpensesList] = useState([])
+
+  useEffect(() => {
+    getBudgetList();
   }, [user])
 
-    /**
-     * Used to get Budget List
-     */
-    const getBudgetList=async()=>{
+  /**
+   * Used to get Budget List
+   */
+  const getBudgetList = async () => {
 
-        const result=await db.select({
-            ...getTableColumns(Budgets),
-            totalSpend: sql`COALESCE(sum(CAST(${Expenses.amount} AS decimal)), 0)`.mapWith(Number),
-            totalItem: sql `count(${Expenses.id})`.mapWith(Number)
-        }).from(Budgets)
-        .leftJoin(Expenses,eq(Budgets.id, Expenses.budgetId))
-        .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress))
-        .groupBy(Budgets.id)
-        .orderBy(desc(Budgets.id));
-        
-        setBudgetList(result);
-        getAllExpenses();
+    const result = await db.select({
+      ...getTableColumns(Budgets),
+      totalSpend: sql`COALESCE(sum(CAST(${Expenses.amount} AS decimal)), 0)`.mapWith(Number),
+      totalItem: sql`count(${Expenses.id})`.mapWith(Number)
+    }).from(Budgets)
+      .leftJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress))
+      .groupBy(Budgets.id)
+      .orderBy(desc(Budgets.id));
 
-    }
+    setBudgetList(result);
+    getAllExpenses();
 
-    const getAllExpenses=async()=>{
-      const result=await db.select({
-        id:Expenses.id,
-        name:Expenses.name,
-        amount:Expenses.amount,
-        createdAt:Expenses.createdAt
-      }).from(Budgets)
-      .rightJoin(Expenses,eq(Budgets.id, Expenses.budgetId))
-      .where(eq(Budgets.createdBy,user?.primaryEmailAddress.emailAddress))
+  }
+
+  const getAllExpenses = async () => {
+    const result = await db.select({
+      id: Expenses.id,
+      name: Expenses.name,
+      amount: Expenses.amount,
+      createdAt: Expenses.createdAt
+    }).from(Budgets)
+      .rightJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress.emailAddress))
       .orderBy(desc(Expenses.id));
-      setExpensesList(result);
-    }
+    setExpensesList(result);
+  }
 
   return (
-    <div className='p-8'>
-      <h2 className='font-bold text-3xl'>
-      Hi, {user?.firstName} 😊</h2>
-      <p className='text-gray-500'>Here's what's happening with your money, let's manage your expenses!</p>
-      <CardInfo budgetList={budgetList}/>
-      <div className='grid grid-cols-1 md:grid-cols-3 mt-6 gap-5'>
-        <div className='md:col-span-2'>
-          <BarChartDashboard
-            budgetList={budgetList}
-          />
-          <ExpenseListTable
-          expensesList={expensesList}
-          refreshData={()=>getBudgetList}/>
-        </div>
-        <div className='grid gap-5'>
-          <h2 className='font-bold text-lg'>Latest Budgets</h2>
-          {budgetList.map((budget,index)=>(
-            <BudgetItem budget={budget} key={index}/>
-          ))}
+    <>
+      <div className='p-8'>
+        <h2 className='font-bold text-3xl'>
+          Hi, {user?.firstName} 😊</h2>
+        <p className='text-gray-500'>Here's what's happening with your money, let's manage your expenses!</p>
+        <CardInfo budgetList={budgetList} />
+        <div className='grid grid-cols-1 md:grid-cols-3 mt-6 gap-5'>
+          <div className='md:col-span-2'>
+            <BarChartDashboard
+              budgetList={budgetList}
+            />
+            <ExpenseListTable
+              expensesList={expensesList}
+              refreshData={() => getBudgetList} />
+          </div>
+          <div className='grid gap-5'>
+            <h2 className='font-bold text-lg'>Latest Budgets</h2>
+            {budgetList.map((budget, index) => (
+              <BudgetItem budget={budget} key={index} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+      <Chat />
+    </>
   )
 }
 
