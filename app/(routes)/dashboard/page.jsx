1f1af85b0e2847246339564d1718a87cfe21e8ee
agnes <a.tjokrosetio@student.unsw.edu.au>
2025-03-15 21:11:41 +1100

    adding donations tab to the side menu

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -1,34 +1,35 @@
-"use client"
-import { UserButton, useUser } from '@clerk/nextjs'
-import React, { useEffect, useState } from 'react'
-import CardInfo from './_components/CardInfo';
-import { db } from '@/utils/dbConfig';
-import { desc, eq, getTableColumns, sql } from 'drizzle-orm';
-import { Budgets, Expenses } from '@/utils/schema';
-import BarChartDashboard from './_components/BarChartDashboard';
-import BudgetItem from './budgets/_components/BudgetItem';
-import ExpenseListTable from './expenses/_components/ExpenseListTable';
-import Chat from './Chatbot/Chat';
+"use client";
+import { UserButton, useUser } from "@clerk/nextjs";
+import React, { useEffect, useState } from "react";
+import CardInfo from "./_components/CardInfo";
+import { db } from "@/utils/dbConfig";
+import { desc, eq, getTableColumns, sql } from "drizzle-orm";
+import { Budgets, Expenses } from "@/utils/schema";
+import BarChartDashboard from "./_components/BarChartDashboard";
+import BudgetItem from "./budgets/_components/BudgetItem";
+import ExpenseListTable from "./expenses/_components/ExpenseListTable";
+import Chat from "./Chatbot/Chat";
 
 function Dashboard() {
   const { user } = useUser();
   const [budgetList, setBudgetList] = useState([]);
-  const [expensesList, setExpensesList] = useState([])
+  const [expensesList, setExpensesList] = useState([]);
 
   useEffect(() => {
     getBudgetList();
-  }, [user])
+  }, [user]);
 
   /**
    * Used to get Budget List
    */
   const getBudgetList = async () => {
-
-    const result = await db.select({
-      ...getTableColumns(Budgets),
-      totalSpend: sql`COALESCE(sum(CAST(${Expenses.amount} AS decimal)), 0)`.mapWith(Number),
-      totalItem: sql`count(${Expenses.id})`.mapWith(Number)
-    }).from(Budgets)
+    const result = await db
+      .select({
+        ...getTableColumns(Budgets),
+        totalSpend: sql`COALESCE(sum(CAST(${Expenses.amount} AS decimal)), 0)`.mapWith(Number),
+        totalItem: sql`count(${Expenses.id})`.mapWith(Number),
+      })
+      .from(Budgets)
       .leftJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
       .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress))
       .groupBy(Budgets.id)
@@ -36,40 +37,36 @@ function Dashboard() {
 
     setBudgetList(result);
     getAllExpenses();
-
-  }
+  };
 
   const getAllExpenses = async () => {
-    const result = await db.select({
-      id: Expenses.id,
-      name: Expenses.name,
-      amount: Expenses.amount,
-      createdAt: Expenses.createdAt
-    }).from(Budgets)
+    const result = await db
+      .select({
+        id: Expenses.id,
+        name: Expenses.name,
+        amount: Expenses.amount,
+        createdAt: Expenses.createdAt,
+      })
+      .from(Budgets)
       .rightJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
       .where(eq(Budgets.createdBy, user?.primaryEmailAddress.emailAddress))
       .orderBy(desc(Expenses.id));
     setExpensesList(result);
-  }
+  };
 
   return (
     <>
-      <div className='p-8'>
-        <h2 className='font-bold text-3xl'>
-          Hi, {user?.firstName} 😊</h2>
-        <p className='text-gray-500'>Here's what's happening with your money, let's manage your expenses!</p>
+      <div className="p-8">
+        <h2 className="font-bold text-3xl">Hi, {user?.firstName} 😊</h2>
+        <p className="text-gray-500">Here's what's happening with your money, let's manage your expenses!</p>
         <CardInfo budgetList={budgetList} />
-        <div className='grid grid-cols-1 md:grid-cols-3 mt-6 gap-5'>
-          <div className='md:col-span-2'>
-            <BarChartDashboard
-              budgetList={budgetList}
-            />
-            <ExpenseListTable
-              expensesList={expensesList}
-              refreshData={() => getBudgetList} />
+        <div className="grid grid-cols-1 md:grid-cols-3 mt-6 gap-5">
+          <div className="md:col-span-2">
+            <BarChartDashboard budgetList={budgetList} />
+            <ExpenseListTable expensesList={expensesList} refreshData={() => getBudgetList} />
           </div>
-          <div className='grid gap-5'>
-            <h2 className='font-bold text-lg'>Latest Budgets</h2>
+          <div className="grid gap-5">
+            <h2 className="font-bold text-lg">Latest Budgets</h2>
             {budgetList.map((budget, index) => (
               <BudgetItem budget={budget} key={index} />
             ))}
@@ -78,7 +75,7 @@ function Dashboard() {
       </div>
       <Chat />
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
