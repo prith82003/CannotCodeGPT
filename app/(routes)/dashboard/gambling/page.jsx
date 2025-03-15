741db80f67aca17cb15b32353d6b6348503c12d1
Steve Thomas <z5418430@ad.unsw.edu.au>
2025-03-15 23:14:18 +1100

    added bot to gambling

--- app/(routes)/dashboard/gambling/page.jsx
+++ app/(routes)/dashboard/gambling/page.jsx
@@ -1,14 +1,17 @@
 "use client"
 import { useState } from 'react'
 import GamblingTab from "./_components/GamblingTab";
-import './Gambling.css'
+import './gambling.css'
+import Chat from '../Chatbot/Chat';
 
 function Gambling() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
     <GamblingTab />
-
+    <Chat />
+    </>
   )
 }
 
